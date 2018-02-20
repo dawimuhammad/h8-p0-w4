9b5876b4b49dd3824d396d6d4c1e07f28a8ab6f8
dawimuhammad <muhamadhaddawi@gmail.com>
2018-02-21 00:20:24 +0700

    add exercise 5 - ubah kata

---
+++ 5-ubahkata.js
@@ -0,0 +1,20 @@
+//  Exercise 4 Week 4 Case 'Mengubah Kata'
+//  by Muhamad Haddawi Hacktiv8 Student Phase 0 Batch QFox
+
+function ubahHuruf(kata) {
+    var abjad = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var i=0;
+    var kataBaru = '';
+
+    for (i; i<kata.length; i++) {
+        var j = 0;
+        for(j; j<abjad.length; j++) {
+            if (kata.charAt(i) === abjad[j]) {
+                kataBaru = kataBaru + abjad[j+1];
+                j = abjad.length;
+            }
+        }
+    }
+
+    return kataBaru;
+}
