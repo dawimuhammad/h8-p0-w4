8735c138194b16986ec984f1623b945e71c3503d
dawimuhammad <muhamadhaddawi@gmail.com>
2018-02-22 01:05:45 +0700

    add variabel inside function

--- 5-ubahkata.js
+++ 5-ubahkata.js
@@ -18,3 +18,10 @@ function ubahHuruf(kata) {
 
     return kataBaru;
 }
+
+// TEST CASES
+console.log(ubahHuruf('wow')); // xpx
+console.log(ubahHuruf('developer')); // efwfmpqfs
+console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+console.log(ubahHuruf('keren')); // lfsfo
+console.log(ubahHuruf('semangat')); // tfnbohbu
